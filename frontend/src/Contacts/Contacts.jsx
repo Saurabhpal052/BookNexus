cf6eebf6288c40b9b17eb5578d3a196632ae19e2
saurabhpal052 <saurabhpal811@gmail.com>
2024-11-22 18:57:37 +0530

    Updated

--- frontend/src/Contacts/Contacts.jsx
+++ frontend/src/Contacts/Contacts.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Contact from '../components/Contact'
 import Navbar from '../components/Navbar'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 function Contacts() {
   return (
     <div>
